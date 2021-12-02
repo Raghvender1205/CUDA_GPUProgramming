15d9adc976847750b93f17dfbc9b317c08ec7c08
Raghvender <raghvender1205@gmail.com>
2021-12-03 02:25:38 +0530

    Point Concept

---
+++ C++ AI DeepLearning/Util/Point.cpp
@@ -0,0 +1,13 @@
+#include "Point.hpp"
+
+namespace AI {
+    Point::Point() {
+        x = 0;
+        y = 0;
+    }
+
+    Point::Point(int x, int y) {
+        this->x = x;
+        this->y = y;
+    }
+}
---
+++ C++ AI DeepLearning/Util/Point.hpp
@@ -0,0 +1,14 @@
+#ifndef POINT_HPP
+#define POINT_HPP
+
+namespace AI {
+    class Point {
+        public:
+            Point();
+            Point(int x, int y);
+
+            int x, y;
+    };
+}
+
+#endif // POINT_HPP
